0d5a185abb5d45c11cdc182d4cc22949ae9a9ccc
RezaEbrahimi81 <r3z4.3br4h1m1@gmail.com>
2024-08-20 05:47:25 +0330

    dark mode

---
+++ Example.js
@@ -0,0 +1,9 @@
+let body =
+document.querySelector("body");
+let isToggled = false;
+ 
+body.onclick = () => {
+    isToggled = !isToggled;
+    body.style.setProperty("--value",
+      isToggled ? "black" : "white")
+}
